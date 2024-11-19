a588708e26d78ebaa869eb43161b5509bab791c5
Rotem Sharom <rotemts10@gmail.com>
2024-11-19 08:08:08 +0200

    add about page

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ app.get('/', (req, res) => {
 //    res.sendFile(path.join(__dirname, 'views/index.html'))
 // })
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/about.html'))
+})
+
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`)
 })
